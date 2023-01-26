dfd0b97562618e84e6adb5a594fe8e4d48f96690
charliespy <42754940+charliespy@users.noreply.github.com>
2023-01-26 10:46:04 -0800

    commit after fixing reverses

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  @Test 
+  public void testReverseInPlace2() {
+    int[] input1 = { 0, 1, 2, 3, 4, 5 };
+    int[] result1 = ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5, 4, 3, 2, 1, 0 }, result1);
+  }
+
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 0, 1, 2, 3, 4 };
+    int[] result1 = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{ 4, 3, 2, 1, 0 }, result1);
+  }
 }
